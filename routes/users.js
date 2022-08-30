516b5e4f8630eca342becead01575f6c77ad77c5
Sujana Bhandari <sujana6589@gmail.com>
2022-08-30 16:17:25 +0200

    Display the list of pokemons

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
+
 var router = express.Router();
+
+const {
+  get_all_pokemon
+} = require("../controllers/pokemon");
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
 
 module.exports = router;
