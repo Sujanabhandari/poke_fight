61e9977e1e83e034d47380bcf4915e07d3a089f1
Olaoluwa Lewis <olewis4@web.de>
2022-09-07 18:16:08 +0200

    level 3 n 4

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 const { get_all_pokemon, get_all_pokemon_id, get_all_pokemon_id_info } = require("../controllers/pokemon");
-
+const { display_game_result, display_leaderboard } = require("../controllers/game");
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
@@ -13,5 +13,10 @@ router.route("/pokemons").get(get_all_pokemon);
 router.route("/pokemons/:id").get(get_all_pokemon_id);
 
 router.route("/pokemons/:id/:info").get(get_all_pokemon_id_info);
+
+router.route("/leaderboard").get(display_game_result);
+
+router.route("/game/leaderboard").get(display_leaderboard);
+
 
 module.exports = router;
